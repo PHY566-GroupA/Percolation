e3b2312b0aa0169250957dd277aa4a759ed139ba
ankursg8 <ankur.manikandan@duke.edu>
2015-04-10 22:43:04 -0400

    Update percolation.py

--- percolation.py
+++ percolation.py
@@ -69,20 +69,17 @@ def percolation(num1,num2,xcluster,ycluster,grid):
 
 def check(xcluster,ycluster,n):
 	'''
-	check the existence of spanning cluster 
+	check the existence of spanning cluster
 	'''
 	ans = 1
 	num = 0
 	for i in range(len(xcluster)):
 		if (len(xcluster[i])!=0):
-			if(min(xcluster[i])==0 and max(xcluster[i])==n-1):	
-				num  = i
-				ans = 0		
-				break			
-			if(min(ycluster[i])==0 and max(ycluster[i])==n-1):
-				num = i
-				ans = 0
-				break
+			if(min(xcluster[i])==0 and max(xcluster[i])==n-1):
+				if(min(ycluster[i])==0 and max(ycluster[i])==n-1):
+					num = i
+					ans = 0
+					break
 	return ans, num, len(xcluster[num])
 
 def fit_func(x, a, b):
